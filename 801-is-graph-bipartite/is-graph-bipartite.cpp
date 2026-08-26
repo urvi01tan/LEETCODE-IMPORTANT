cd2c59b9a0730cbb062a321e335e4f5f89227cc7
URVASHI TANWAR <167461316+urvi01tan@users.noreply.github.com>
2026-08-26 20:55:43 +0530

    Time: 11 ms (5.09%) | Memory: 18 MB (8.61%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,42 +1,36 @@
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        bool canwe=false;
-    int n=graph.size();
-    for(int i=0;i<n;i++)
+bool dfs(vector<vector<int>>&graph,int node,vector<int>&c)
+{
+   // v[node]=1;
+   // if(parent==-1)c[node]=0;
+
+    for(auto it:graph[node])
     {
-        vector<int>c(n+1,-1);
-        queue<int>pq;
-        pq.push(i);
-        c[i]=0;
-        while(!pq.empty())
+        if(c[it]==-1)
         {
-            int node=pq.front();
-            pq.pop();
-            for(auto it:graph[node])
-            {
-                if(c[it]==-1)
-                {
-                    if(c[node]==0)
-                    {
-                        c[it]=1;
-                        pq.push(it);
-                    }
-                    else
-                    {
-                        c[it]=0;
-                        pq.push(it);
-                    }
-                }
-                else
-                {
-                    if(c[it]==c[node])return false;
-                    else
-                    continue;
-                }
-            }
+            if(c[node]==0)c[it]=1;
+            else
+            c[it]=0;
+           if(! dfs(graph,it,c))return false;
+        }
+        else
+        {
+            if(c[node]==c[it])return false;
+
         }
     }
     return true;
-    }
+}
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n=graph.size();
+        //vector<int>v(n+1,0);
+        
+        for(int i=0;i<n;i++)
+        { vector<int>c(n+1,-1);
+        c[i]=0;
+            if(!dfs(graph,i,c))return false;
+        }
+        return true;
+        }
 };
