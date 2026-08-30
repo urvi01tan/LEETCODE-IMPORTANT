f145c1df338d93d5702ad4d398a9a53cee823672
URVASHI TANWAR <167461316+urvi01tan@users.noreply.github.com>
2026-08-30 19:28:55 +0530

    Time: 15 ms (5.65%) | Memory: 10.6 MB (51.28%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+//generate permutation through backtractikkng
+void fun(int index,vector<int>&v,vector<int>&ans,vector<int>&nums,vector<vector<int>>&re)
+{
+    if(ans.size()==nums.size())
+    {re.push_back(ans);
+    return;}
+    if(index==nums.size())return;
+    for(int i=0;i<nums.size();i++)
+    {
+        if(v[i]==0 && i!=index)
+        {
+            v[i]=1;
+            cout<<nums[i]<<"  "<<endl;
+            ans.push_back(nums[i]);
+            fun(i,v,ans,nums,re);
+            ans.pop_back();
+            v[i]=0;
+        }
+    }
+    return ;
+}
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans;
+        vector<int>v(n,0);
+        vector<vector<int>>re;
+        fun(-1,v,ans,nums,re);
+        return re;
+    }
+};
