27b1305f0d8cec55ed92c985bfe58cbf0cf3c4d4
URVASHI TANWAR <167461316+urvi01tan@users.noreply.github.com>
2026-08-30 12:29:09 +0530

    Time: 0 ms (100%), Space: 125 MB (47.37%) - LeetHub

--- 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     int minDeletion(vector<int>& nums) {
        int n=nums.size();
-       if(nums.size()==1)return 1;
+       if(n==1)return 1;
        //delet as max adjacent 
        int d=0;
        for(int i=0;i<n-1;i++)
        {
         int index=i-d;
         if(index%2==0 && nums[i]==nums[i+1])
-        { cout<<"deleted:"<<i<<endl;
+        { 
             d++;
         }
        } 
        int left=n-d;
-       if(left%2!=0)d++;
+       if(left%2!=0)return d+1;
        return d;
     }
 };
