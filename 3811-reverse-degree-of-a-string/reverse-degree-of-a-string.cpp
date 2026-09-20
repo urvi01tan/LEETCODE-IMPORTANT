e13449943d440f11ab5821ae633daecf7bb8e5e3
URVASHI TANWAR <167461316+urvi01tan@users.noreply.github.com>
2026-09-20 11:24:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (78.39%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int reverseDegree(string s) {
-        int n=s.size();
         int ans=0;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<s.size();i++)
         {
-            int k=(i+1)*(26-(s[i]-'a'));
-
-            ans=ans+k;
+               ans=ans+(i+1)*(26-(s[i]-'a'));
         }
         return ans;
     }
