66c5b4f5ff99903026e348882dc88f5b62d4099a
URVASHI TANWAR <167461316+urvi01tan@users.noreply.github.com>
2026-09-24 01:33:58 +0530

    Time: 3 ms (3.88%), Space: 8.8 MB (39.36%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,60 +1,44 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        //if(pattern.size()!=s.size())return false;
-       vector<string>vec;
-       string k="";
-       for(auto it:s)
-       {
-        if(it==' ')
-        {
-            vec.push_back(k);
-            k="";
+        // if(pattern.size()!=s.size())return false;
+        vector<string> vec;
+        string k = "";
+        for (auto it : s) {
+            if (it == ' ') {
+                vec.push_back(k);
+                k = "";
+            } else {
+                k = k + it;
+            }
         }
-        else
-        {
-            k=k+it;
-        }
-       } 
-       vec.push_back(k);
-       if(pattern.size()!=vec.size())return false;
-       for(auto it:vec)
-       cout<<it<<" ";
-       unordered_map<char,string>mp;
-        unordered_map<string,char>mp2;
-       for(int i=0;i<pattern.size();i++)
-       {
-        if(mp.find(pattern[i])==mp.end())
-        {
-            /////daallllldddooooooooo
-            mp[pattern[i]]=vec[i];
-            cout<<"neww:"<<pattern[i]<<" "<<vec[i]<<endl;
-        }
-        else
-        { cout<<"old:"<<pattern[i]<<" "<<vec[i]<<endl;
-            if(mp[pattern[i]]==vec[i])continue;
-            else
+        vec.push_back(k);
+        if (pattern.size() != vec.size())
             return false;
-        }
-       }
+        unordered_map<char, string> mp;
+        unordered_map<string, char> mp2;
+        for (int i = 0; i < pattern.size(); i++) {
+            if (mp.find(pattern[i]) == mp.end()) {
+                /////daallllldddooooooooo
+                mp[pattern[i]] = vec[i];
+            } else {
+                if (mp[pattern[i]] == vec[i])
+                    continue;
+                else
+                    return false;
+            }
+            if (mp2.find(vec[i]) == mp2.end()) {
+                /////daallllldddooooooooo
+                mp2[vec[i]] = pattern[i];
 
-
-
-         for(int i=0;i<vec.size();i++)
-       {
-        if(mp2.find(vec[i])==mp2.end())
-        {
-            /////daallllldddooooooooo
-            mp2[vec[i]]=pattern[i];
-            cout<<"neww:"<<pattern[i]<<" "<<vec[i]<<endl;
+            } else {
+                if (mp2[vec[i]] == pattern[i])
+                    continue;
+                else
+                    return false;
+            }
         }
-        else
-        { cout<<"old:"<<pattern[i]<<" "<<vec[i]<<endl;
-            if(mp2[vec[i]]==pattern[i])continue;
-            else
-            return false;
-        }
-       }
-       return true;
+
+        return true;
     }
 };
