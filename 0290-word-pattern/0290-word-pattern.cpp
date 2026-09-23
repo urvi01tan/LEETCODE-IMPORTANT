ff4165c8e31d497c5fa7fa02eb006df5bd8edfbc
URVASHI TANWAR <167461316+urvi01tan@users.noreply.github.com>
2026-09-24 01:34:54 +0530

    Time: 0 ms (100%), Space: 8.7 MB (91.72%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        // if(pattern.size()!=s.size())return false;
-        vector<string> vec;
-        string k = "";
-        for (auto it : s) {
-            if (it == ' ') {
-                vec.push_back(k);
-                k = "";
+        vector<string> words;
+        string word;
+
+        for (char c : s) {
+            if (c == ' ') {
+                words.push_back(word);
+                word = "";
             } else {
-                k = k + it;
+                word += c;
             }
         }
-        vec.push_back(k);
-        if (pattern.size() != vec.size())
+        words.push_back(word);
+
+        if (pattern.size() != words.size())
             return false;
+
         unordered_map<char, string> mp;
         unordered_map<string, char> mp2;
-        for (int i = 0; i < pattern.size(); i++) {
-            if (mp.find(pattern[i]) == mp.end()) {
-                /////daallllldddooooooooo
-                mp[pattern[i]] = vec[i];
-            } else {
-                if (mp[pattern[i]] == vec[i])
-                    continue;
-                else
-                    return false;
-            }
-            if (mp2.find(vec[i]) == mp2.end()) {
-                /////daallllldddooooooooo
-                mp2[vec[i]] = pattern[i];
 
-            } else {
-                if (mp2[vec[i]] == pattern[i])
-                    continue;
-                else
-                    return false;
-            }
+        for (int i = 0; i < pattern.size(); i++) {
+            char c = pattern[i];
+
+            // Existing mapping must be consistent
+            if (mp.count(c) && mp[c] != words[i])
+                return false;
+
+            // Existing word must map to the same character
+            if (mp2.count(words[i]) && mp2[words[i]] != c)
+                return false;
+
+            mp[c] = words[i];
+            mp2[words[i]] = c;
         }
 
         return true;
